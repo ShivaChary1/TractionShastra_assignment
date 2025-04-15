76cecdd9a6838b3fb2423dd96a1400ab0105842e
Shiva Chary <158066697+ShivaChary1@users.noreply.github.com>
2025-04-15 14:01:01 +0530

    Update db_ connect.php

--- config/db_ connect.php
+++ config/db_ connect.php
@@ -1,1 +1,13 @@
+<?php
+$host = 'localhost';
+$db = 'task_app';
+$user = 'root';
+$pass = '';
 
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
